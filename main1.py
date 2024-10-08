270930cf834ce2eb18c955ab17827989ee775142
Bred <brederman1984@gmail.com>
2024-10-08 10:27:11 +0300

    Класс print

--- main1.py
+++ main1.py
@@ -1,8 +1,9 @@
 print('1111')
 print(1234)
-class h:
-    def printt(self,s:str):
+class pprint:
+    @staticmethod
+    def print(s:str):
         print(s)
 
-p=h()
-p.printt('sdcsadc11')
+
+pprint.print('asdc')
