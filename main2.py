ba5207d8148c0cd779357d5db60ead7b82cbbb41
Bred <brederman1984@gmail.com>
2024-10-08 20:02:22 +0300

    Класс print1

--- main2.py
+++ main2.py
@@ -1,3 +1,5 @@
+import json
+
 
 class pprint:
     @staticmethod
@@ -7,5 +9,30 @@ class pprint:
     @staticmethod
     def print_dict(ddd:dict):
         pass
+import os
+# import json
+# pprint.print('as111dc')
+#
+# str1='''{"cars":[{"model":"audi"},{"model":"bmw"}]}'''
+# d=json.loads(str1)
+# print(d,type(d))
+#
+# sttt=json.dumps(d)
+# print(sttt,type(sttt))
 
-pprint.print('as111dc')
+# if not os.path.isfile('test1.txt'):
+#     open('test1.txt','w')
+#
+# with open('test1.txt','a') as f:
+#     f.writelines('sdwc')
+
+import requests
+
+r=requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+
+str11=json.loads(r.text)
+print(str11['Date'])
+print(r.json()['Date'])
+
+with open('cb.json','w') as f:
+    json.dump(r.json(),f,indent=4)
